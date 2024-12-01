1985718d09378d6d4c453c40423cd716bb911f47
Itamar0510 <itamar0510@mevoot.co.il>
2024-12-01 17:12:59 +0200

    ITAMAR2

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 
 # Initialize Pygame
 pygame.init()
-print("Maman is the best !!!!")
 
 # Set up display
 WIDTH, HEIGHT = 800, 600
@@ -11,6 +10,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Moving Background")
 
 # Load background image
+
+
+print("ITAMAR!!!")
 background = pygame.image.load('background_image.jpg')
 background_rect = background.get_rect()
 
